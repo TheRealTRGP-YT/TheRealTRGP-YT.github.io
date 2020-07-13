3db138b48dd07681391654b22aa7009fe3931276
TheRealTRGP-YT <Donna1968@Jamess-MacBook-Pro.local>
2020-07-13 18:48:52 +1000

    Updated index.*

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function add(a, b) {
     return a + b;
 }
+
+function Get(id) {
+    let a = document.getElementById(id);
+}
+
+function GetAdd(id, id2, id3) {
+    let a = document.getElementById(id).innerHTML;
+    console.log(a);
+    let b = document.getElementById(id2).innerHTML;
+    console.log(b);
+    let c = document.getElementById(id3).innerHTML = a + b;
+    console.log(c);
+
+
+}
